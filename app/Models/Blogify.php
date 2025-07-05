c89d0f247c6f7c3fee0086527c9ddc928aa1ca51
Odinbytes <jabwunza@gmail.com>
2025-07-05 12:31:21 +0300

    Moved project files to root

---
+++ app/Models/Blogify.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blogify extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'content',
+        'image',
+        'user_id'
+    ];
+
+    public function user (){
+        return $this->belongsTo(User::class);
+    }
+}
